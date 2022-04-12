45cce2a37126b9acf09b287ba4b80f6ec004ac79
han253 <han315730@gmail.com>
2022-04-12 18:28:40 -0500

    add UIS to title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,9 +6,9 @@
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
-          Title
+          UIS IoT
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
@@ -16,7 +16,6 @@
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
       <q-scroll-area class="fit">
         <q-list>
-
           <template v-for="(menuItem, index) in menuList" :key="index">
             <q-separator :key="'sep' + index" v-if="menuItem.separator" />
             <q-item :to="menuItem.to" v-else clickable v-ripple>
@@ -28,7 +27,6 @@
               </q-item-section>
             </q-item>
           </template>
-
         </q-list>
       </q-scroll-area>
     </q-drawer>
@@ -44,17 +42,16 @@
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
-
   </q-layout>
 </template>
 
 <script lang="ts" setup>
-import { menuList } from 'src/models/menu'
-import { ref } from 'vue'
+  import { menuList } from 'src/models/menu'
+  import { ref } from 'vue'
 
-const leftDrawerOpen = ref(false)
+  const leftDrawerOpen = ref(false)
 
-const toggleLeftDrawer = () => {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
+  const toggleLeftDrawer = () => {
+    leftDrawerOpen.value = !leftDrawerOpen.value
+  }
 </script>
